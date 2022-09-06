d12c3016a501d5d0098358d0cb61f94a6f8bc1e8
Vivian <davidvivian201@gmail.com>
2022-09-06 13:57:07 +0100

    Designing the message activity UI

--- AndroidStudioProjects/WMTApp/app/src/main/java/com/example/wmtapp/ChatActivity.kt
+++ AndroidStudioProjects/WMTApp/app/src/main/java/com/example/wmtapp/ChatActivity.kt
@@ -3,16 +3,27 @@ package com.example.wmtapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ChatActivity : AppCompatActivity() {
     private lateinit var toolbar: Toolbar
+    private lateinit var messageRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
+        setupToolbar()
+
+        messageRecyclerView = findViewById(R.id.messageSection)
+        messageRecyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun setupToolbar() {
         toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
         supportActionBar?.title = "Username"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
