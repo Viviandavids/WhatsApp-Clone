f526319ff2c5ad8ab94ee784105728df45425f31
Vivian <davidvivian201@gmail.com>
2022-09-20 13:41:15 +0100

    Implementation of the Splashscreen

--- AndroidStudioProjects/WMTApp/app/src/main/java/com/example/wmtapp/MainActivity.kt
+++ AndroidStudioProjects/WMTApp/app/src/main/java/com/example/wmtapp/MainActivity.kt
@@ -4,26 +4,26 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.viewpager2.widget.ViewPager2
 import com.example.wmtapp.adapter.MainPageAdapter
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import androidx.appcompat.widget.Toolbar
-import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
 
 class MainActivity : AppCompatActivity() {
     //This is an initialization of the Toolbar
     private lateinit var toolbar: Toolbar
     //This is an initialization of the TabLayout
-    private lateinit var tablayout: TabLayout
+    private lateinit var tabLayout: TabLayout
     //This is an initialization of the ViewPager2
     private lateinit var viewPager: ViewPager2
     private lateinit var context: Context
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -38,17 +38,17 @@ class MainActivity : AppCompatActivity() {
      // We refactored the tabLayout with a function init
     private fun initTabLayout() {
         // This finds the TabLayout by its Id and makes it accessible
-        tablayout = findViewById(R.id.tab)
+        tabLayout = findViewById(R.id.tab)
         // This finds the ViewPager by its Id and makes it accessible
         viewPager = findViewById(R.id.viewPager)
         // To connect the ViewPager to the MainPageAdapter
         viewPager.adapter = MainPageAdapter(supportFragmentManager, lifecycle)
         // To connect the TabLayout to the ViewPager inorder to update the current fragment
 
-        TabLayoutMediator(tablayout, viewPager) { tab, position ->
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             // When condition  is used to set the name option on the TabLayout  based on the position
             when (position) {
-                0 -> tab.icon = getDrawable(R.drawable.ic_baseline_photo_camera_24)
+                0 -> tab.icon = AppCompatResources.getDrawable(context, R.drawable.ic_baseline_photo_camera_24)
                 1 -> tab.text = "Chats"
                 2 -> tab.text = "Status"
                 3 -> tab.text = "Calls"
